b32d7c44bdec7f5cfea5954a38d2ae74dfe152ea
winstxnhdw <winstxnhdw@gmail.com>
2024-01-21 17:19:23 +0800

    chore: import `spawn`

--- scripts/generate_token.ts
+++ scripts/generate_token.ts
@@ -1,20 +1,21 @@
+import { spawn } from 'bun'
 import { randomBytes } from 'crypto'
 
 async function main() {
   const generated_token = randomBytes(48).toString('base64url')
   console.log(`[+] Your generated token is ${generated_token}`)
-  const save = prompt('[?] Would you like to use this token? (y/N)\n')?.toLowerCase()
+  const save = prompt('[?] Would you like to use this token? (y/N)')?.toLowerCase()
 
   if (save !== 'y') {
     return
   }
 
-  const process = Bun.spawn(['npx', 'wrangler', 'secret', 'put', 'TOKEN'], {
+  const process = spawn(['npx', 'wrangler', 'secret', 'put', 'TOKEN'], {
     stdin: new TextEncoder().encode(generated_token)
   })
 
   const response = await new Response(process.stdout).text()
-  console.log(response)
+  console.log(`\n${response}`)
 }
 
 void main()
